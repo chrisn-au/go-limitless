74e4c0b6a2c2538e54fa3262e478e5927abf92ff
chrisn-au <chris@ninjablocks.com>
2015-02-22 16:29:05 +1100

    TurnOn example Change to port and hex for old gateway

--- limitless.go
+++ limitless.go
@@ -4,9 +4,7 @@ import (
   "encoding/binary"
   "net"
   "bytes"
-  "github.com/lucasb-eyer/go-colorful"
-  "time"
-)
+  )
 
 type LimitlessController struct {
   Host string `json:"host"`
@@ -37,54 +35,8 @@ const MAX_BRIGHTNESS = 0x1b
 func NewLimitlessMessage() *LimitlessMessage {
   msg := LimitlessMessage{}
   msg.Suffix = 0x55
+  msg.Value = 0x00
   return &msg
-}
-
-func (g *LimitlessGroup) SendColor(c colorful.Color) (error) {
-  h, s, v := c.Hsv()
-  h = 240.0 - h
-  if h < 0 {
-    h = 360.0 + h
-  }
-  scaled_h := uint8(h * 255.0 / 360.0)
-  scaled_v := uint8(v * MAX_BRIGHTNESS)
-
-  var err error
-
-  if scaled_v < 0x02 {
-    return g.Off()
-  // If closer to white then a saturated color :D
-  } else if s < 0.5 {
-    err = g.White()
-    if err != nil {
-      return err
-    }
-  } else {
-    err = g.Activate()
-    if err != nil {
-      return err
-    }
-    time.Sleep(100 * time.Millisecond)
-    err = g.SetHue(scaled_h)
-    if err != nil {
-      return err
-    }
-
-  }
-  err = g.Activate()
-  if err != nil {
-    return err
-  }
-  time.Sleep(100 * time.Millisecond)
-  err = g.SetBri(scaled_v)
-  return err
-}
-
-func (g *LimitlessGroup) SetHue(h uint8) (error) {
-  msg := NewLimitlessMessage()
-  msg.Key = 0x40
-  msg.Value = h
-  return g.Controller.sendMsg(msg)
 }
 
 func (g *LimitlessGroup) SetBri(b uint8) (error) {
@@ -98,19 +50,19 @@ func (g *LimitlessGroup) SetBri(b uint8) (error) {
 }
 func (g *LimitlessGroup) White() (error) {
   msg := NewLimitlessMessage()
-  msg.Key = uint8(0xC5 + ((g.Id - 1) * 2))
+  msg.Key = 0x35 //all on
   return g.Controller.sendMsg(msg)
 }
 
 func (g *LimitlessGroup) On() (error) {
   msg := NewLimitlessMessage()
-  msg.Key = uint8(0x45 + ((g.Id - 1) * 2))
+  msg.Key = 0x35
   return g.Controller.sendMsg(msg)
 }
 
 func (g *LimitlessGroup) Off() (error) {
   msg := NewLimitlessMessage()
-  msg.Key = uint8(0x46 + ((g.Id - 1) * 2))
+  msg.Key = 0x39
   return g.Controller.sendMsg(msg)
 }
 
@@ -125,6 +77,7 @@ func (c *LimitlessController) sendMsg(msg *LimitlessMessage) (error) {
   }
 	buf := new(bytes.Buffer)
 	binary.Write(buf, binary.LittleEndian, msg)
-  _, err = conn.Write(buf.Bytes())
+  untitled folder:
+_, err = conn.Write(buf.Bytes())
   return err
 }
